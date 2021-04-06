6634a268b586ff68c8c4b62d613a72e436a22627
北音执念~ <1031622947@qq.com>
2021-04-06 14:53:40 +0800

    修改sysUserInfo

--- Caviar.Control/ModelController/SysUserInfo.cs
+++ Caviar.Control/ModelController/SysUserInfo.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caviar.Models.SystemData;
 
-namespace Caviar.Models.SystemData
+namespace Caviar.Control
 {
     public class SysUserInfo
     {
