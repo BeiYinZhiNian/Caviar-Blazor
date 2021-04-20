9e51d85cc12f70abedad8c69d1b298010011449c
北音执念~ <1031622947@qq.com>
2021-04-20 19:01:42 +0800

    新增菜单

---
+++ Caviar.UI/Pages/SystemPages/Menu/Add.razor.cs
@@ -0,0 +1,13 @@
+﻿using Caviar.Models.SystemData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Caviar.UI.Pages.SystemPages.Menu
+{
+    public partial class Add
+    {
+        public SysPowerMenu model = new SysPowerMenu();
+    }
+}
