60d51414a1cf46641e9facc6747312b79d90e81e
北音执念~ <1031622947@qq.com>
2021-05-10 11:06:00 +0800

    更改名称为AntDesignPages，将资源移动

---
+++ Caviar.AntDesignPages/wwwroot/Js/Caviar.js
@@ -0,0 +1,32 @@
+﻿/*
+name:cookie 名
+value:cookie 值
+*/
+//写入cookie
+function setCookie(name, value, Days) {
+	var exp = new Date();
+	exp.setTime(exp.getTime() + Days * 60 * 1000);//60*1000=1分钟
+	document.cookie = name + "=" + escape(value) + ";path=/;expires=" + exp.toGMTString();
+}
+
+///删除cookie
+function delCookie(name) {
+	var exp = new Date();
+	exp.setTime(exp.getTime() - 1);
+	var cval = getCookie(name);
+	if (cval != null) document.cookie = name + "=" + cval + ";path=/;expires=" + exp.toGMTString();
+}
+
+// 获取指定名称的cookie
+function getCookie(name) {
+	var strcookie = document.cookie;//获取cookie字符串
+	var arrcookie = strcookie.split("; ");//分割
+	//遍历匹配
+	for (var i = 0; i < arrcookie.length; i++) {
+		var arr = arrcookie[i].split("=");
+		if (arr[0] == name) {
+			return arr[1];
+		}
+	}
+	return "";
+}
