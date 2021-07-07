e8af04e22566c7bbe46ac1b81b52b84db9d6ef7d
震 王 <1031622947@qq.com>
2021-07-07 23:41:13 +0800

    CavUpload

---
+++ Caviar.AntDesignPages/Shared/CavUpload.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caviar.AntDesignPages.Shared
+{
+    public partial class CavUpload
+    {
+        public new string Action { get; set; }
+
+        public new Dictionary<string,string> Headers { get; set; }
+    }
+}
