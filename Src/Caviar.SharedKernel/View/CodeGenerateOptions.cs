77d55c61e840d60931fb155f6b241a226c6cecc0
BeiYinZhiNian <1031622947@qq.com>
2021-10-09 16:28:27 +0800

    代码生成

--- Src/Caviar.SharedKernel/View/CodeGenerateOptions.cs
+++ Src/Caviar.SharedKernel/View/CodeGenerateOptions.cs
@@ -11,11 +11,16 @@ namespace Caviar.SharedKernel.View
     public class CodeGenerateOptions
     {
         [DisplayName("视图名称")]
+        [Required(ErrorMessage = "视图名称名称必填")]
         public string ViewName { get; set; }
         [DisplayName("服务名称")]
+        [Required(ErrorMessage = "服务名称必填")]
         public string ServiceName { get; set; }
         [DisplayName("控制器名称")]
+        [Required(ErrorMessage = "控制器名称必填")]
         public string ControllerName { get; set; }
+        [DisplayName("实体名称")]
+        [Required(ErrorMessage = "实体名称必填")]
         public string EntityName { get; set; }
         [Required(ErrorMessage = "目录名称必填")]
         public string OutName { get; set; }
