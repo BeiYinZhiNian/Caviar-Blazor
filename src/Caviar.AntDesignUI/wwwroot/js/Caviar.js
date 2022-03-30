c24c36fb2d04b4bfd93170c786ec8440d6a7dfe2
震 王 <1031622947@qq.com>
2022-03-30 22:00:31 +0800

    加载优化

--- src/Caviar.AntDesignUI/wwwroot/js/Caviar.js
+++ src/Caviar.AntDesignUI/wwwroot/js/Caviar.js
@@ -130,3 +130,24 @@ window.onbeforeunload = onbeforeunload_handler;
 function onbeforeunload_handler() {
 	console.log(document.activeElement.href);
 }
+
+function loadJS(url, callback) {
+	var script = document.createElement('script'),
+		fn = callback || function () { };
+	script.type = 'text/javascript';
+	if (script.readyState) {
+		script.onreadystatechange = function () {
+			if (script.readyState == 'loaded' || script.readyState == 'complete') {
+				script.onreadystatechange = null;
+				console.log("加载成功")
+			}
+		};
+	} else {
+		//其他浏览器
+		script.onload = function () {
+			console.log("加载成功")
+		};
+	}
+	script.src = url;
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
