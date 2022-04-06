8d47c2a8c1e7cf59268bd6db86c4833d1e787223
BeiYinZhiNian <1031622947@qq.com>
2022-04-06 10:29:44 +0800

    改为异步加载

--- src/Caviar.AntDesignUI/wwwroot/js/caviar.js
+++ src/Caviar.AntDesignUI/wwwroot/js/caviar.js
@@ -133,22 +133,22 @@ function onbeforeunload_handler() {
 
 function loadJS(url, callback) {
 	var script = document.createElement('script'),
-		fn = callback || function () { };
+		fn = callback || function () { console.log("wasm加载成功"); };
 	script.type = 'text/javascript';
 	if (script.readyState) {
 		script.onreadystatechange = function () {
 			if (script.readyState == 'loaded' || script.readyState == 'complete') {
 				script.onreadystatechange = null;
-				console.log("wasm加载成功")
+				fn();
 			}
 		};
 	} else {
 		//其他浏览器
 		script.onload = function () {
-			console.log("wasm加载成功")
+			fn();
 		};
 	}
 	script.src = url;
-	script.defer = 'defer';
+	script.async = 'async';
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
